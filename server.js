13a68d3e5e108ace892b123bc35b5efc96b941d8
JanaJankovic <jana.jankovic@student.um.si>
2022-09-19 02:59:31 +0200

    update server headers

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+  res.header("X-Frame-Options", "SAMEORIGIN");
+  res.header("X-Content-Type-Options", "nosniff");
+  res.header("Permissions-Policy", "geolocation=(self 'https://nomnio-weather-forecast.herokuapp.com/')");
+  res.header("Referrer-Policy", "same-origin");
+  res.header("Content-Security-Policy", "script-src 'self'");
   next();
 });
 
