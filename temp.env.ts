248b0453cc9090e0a0443081bbff68720c4288bd
JanaJankovic <jana.jankovic@student.um.si>
2022-09-19 10:53:08 +0200

    env update

--- temp.env.ts
+++ temp.env.ts
@@ -1,4 +1,4 @@
 export class TempEnv {
-  public static apiKey =  process.env.apiKey;
-  public static geoApiKey = process.env.geoApiKey;
+  public static apiKey = '87c05a0a8d8ea08172b92485c4a60cde';  //process.env.apiKey;
+  public static geoApiKey = 'apLreaun3ZTtyH50aPYDtw==1fqRBkVr2YqYmZdB';//process.env.geoApiKey;
 }
